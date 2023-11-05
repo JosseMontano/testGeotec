2e89b3a8847a65db4bce7927f4cea24447953b24
JosseMontano <josemariazambranamontano123@gmail.com>
2023-11-05 15:25:30 -0400

    Feature_showUser: add pagination

--- src/app/users/pages/listUser.tsx
+++ src/app/users/pages/listUser.tsx
@@ -1,35 +1,86 @@
+//REACT
+import { useState } from "react";
+
 import { ListUser } from "../interfaces/listUser";
 import CardComponent from "../components/card";
-import { Grid } from "@mui/material";
+import { Grid, Box, Button, Typography } from "@mui/material";
 import UseRedirect from "../../../global/hooks/useRedirect";
 import UseFetch from "../../../global/hooks/useFetch";
 import { getUsers } from "../services/listUser";
 
+type opChangePage = "goOn" | "goBack";
+
 const ListUser = () => {
+  // ======== pagination ========
+  const pagination = 12;
+  const [sincePage, setSincePage] = useState(0);
   //get repos
-  const { data } = UseFetch<ListUser[]>({ service: getUsers });
+  const { data } = UseFetch<ListUser[]>({
+    service: getUsers,
+    pagination: pagination,
+    page: sincePage,
+  });
 
   //redirect to repost
   const { handleRedirect } = UseRedirect();
 
+  // ======== change Page ========
+  const handleChangePage = (op: opChangePage) => {
+    if (op == "goOn") {
+      setSincePage(sincePage + 16);
+      return;
+    }
+    setSincePage(sincePage - 16);
+  };
+
   return (
-    <>
+    <Box
+      height={"100vh"}
+      display={"flex"}
+      flexDirection={"column"}
+      justifyContent={"center"}
+    >
       <Grid
         container
         justifyContent={"center"}
         sx={{ gridTemplateColumns: "repeat(4, minmax(330px, 1fr))" }}
       >
-        {data.length > 0 && data.map((v) => (
-          <CardComponent
-            nameUser={v.login}
-            photo={v.avatar_url}
-            pageUser={v.html_url}
-            handleRedirect={handleRedirect}
-            key={v.id}
-          />
-        ))}
+        {data.length > 0 &&
+          data.map((v) => (
+            <CardComponent
+              nameUser={v.login}
+              photo={v.avatar_url}
+              pageUser={v.html_url}
+              handleRedirect={handleRedirect}
+              key={v.id}
+            />
+          ))}
       </Grid>
-    </>
+
+      <Box
+        display={"flex"}
+        justifyContent={"flex-end"}
+        alignItems={"center"}
+        marginRight={1}
+      >
+        <Button
+          size="small"
+          onClick={() => handleChangePage("goBack")}
+          variant="contained"
+          disabled={sincePage / 16 == 0}
+        >
+          {"<"}
+        </Button>
+        <Typography>{sincePage / 16 + 1}</Typography>
+        <Button
+          size="small"
+          onClick={() => handleChangePage("goOn")}
+          variant="contained"
+        >
+          {">"}
+        </Button>
+      </Box>
+    </Box>
   );
 };
 
--- src/app/users/services/listUser.ts
+++ src/app/users/services/listUser.ts
@@ -1,6 +1,7 @@
 import { getRestApi } from "../../../global/utilities/restApi";
 
-export const getUsers = async <T,>() => {
-  const { json, status } = await getRestApi<T>("users");
+export const getUsers = async <T>(perPage: number, since: number) => {
+  const url = `users?per_page=${perPage}&since=${since}`;
+  const { json, status } = await getRestApi<T>(url);
   return { json, status };
 };
